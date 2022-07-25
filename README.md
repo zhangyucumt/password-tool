e2209ceded418d947d2a07657577f74ade89cff1
zhangyu <zhangyucumt@foxmail.com>
2022-07-26 07:30:32 +0800

    feat: add main.go

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # password-tool
+
+##### 功能
+- 设置配置
+- 设置ssh密码
+- 读取ssh密码
+- 直接ssh链接
+- 上传github
+- download GitHub
+- 设置网站密码
+- 读取网站密码
+- 设置公钥和私钥
+
+##### 一些约定
+- 项目目录
+    ~/.password-tool
+    | - config.yaml
+    | - repo
+      | - default
+        | - .config
+        | - data
+            | - ssh
+                | - a.yaml
+                | - b.yaml
+            | - website
+                | - a.yaml
+                | - b.yaml 
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/urfave/cli/v2"
@@ -10,15 +9,149 @@ import (
 
 func main() {
 	app := &cli.App{
-		Name:  "greet",
-		Usage: "fight the loneliness!",
-		Action: func(*cli.Context) error {
-			fmt.Println("Hello friend!")
-			return nil
+		Commands: []*cli.Command{
+			{
+				Name:    "repo",
+				Aliases: []string{"r"},
+				Usage:   "数据存储的代码仓库",
+				Subcommands: []*cli.Command{
+					{
+						Name:  "add",
+						Usage: "新增数据存储的代码仓库",
+						Flags: []cli.Flag{
+							&cli.BoolFlag{
+								Name:  "default",
+								Value: true,
+								Usage: "是否是默认的代码仓库",
+							},
+							&cli.StringFlag{
+								Name:        "url",
+								Usage:       "代码仓库的地址。默认应具有代码仓库的读写权限",
+								Required:    true,
+								DefaultText: "git@github.com:zhangyucumt/password-tool.git",
+							},
+							&cli.StringFlag{
+								Name:        "name",
+								Usage:       "代码仓库的名称标识。",
+								Required:    true,
+								DefaultText: "repo1",
+							},
+						},
+						Action: func(cCtx *cli.Context) error {
+							fmt.Println("new task template: ", cCtx.Args().First())
+							return nil
+						},
+					},
+					{
+						Name:  "remove",
+						Usage: "移除数据存储的代码仓库",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name:        "name",
+								Usage:       "代码仓库的名称标识。",
+								DefaultText: "repo1",
+							},
+						},
+						Action: func(cCtx *cli.Context) error {
+							fmt.Println("removed task template: ", cCtx.Args().First())
+							return nil
+						},
+					},
+					{
+						Name:  "list",
+						Usage: "数据存储的代码仓库列表",
+						Action: func(cCtx *cli.Context) error {
+							fmt.Println("removed task template: ", cCtx.Args().First())
+							return nil
+						},
+					},
+					{
+						Name:  "set-default",
+						Usage: "设置某个代码仓库为默认的数据存储仓库",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name:        "name",
+								Usage:       "代码仓库的名称标识。",
+								Required:    true,
+								DefaultText: "repo1",
+							},
+						},
+						Action: func(cCtx *cli.Context) error {
+							fmt.Println("removed task template: ", cCtx.Args().First())
+							return nil
+						},
+					},
+					{
+						Name:  "upload",
+						Usage: "将本地存储的数据同步至代码仓库。默认同步的仓库是默认仓库",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name:        "name",
+								Usage:       "需要同步的代码仓库的名称标识。",
+								DefaultText: "repo1",
+							},
+						},
+						Action: func(cCtx *cli.Context) error {
+							fmt.Println("removed task template: ", cCtx.Args().First())
+							return nil
+						},
+					},
+				},
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("removed task template: ", cCtx.Args().First())
+					return nil
+				},
+			},
+			{
+				Name:  "ssh",
+				Usage: "ssh",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "lang",
+						Value:    "english",
+						Usage:    "language for the greeting",
+						Required: true,
+					},
+				},
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("added task: ", cCtx.Args().First())
+					return nil
+				},
+			},
+			{
+				Name:    "complete",
+				Aliases: []string{"c"},
+				Usage:   "complete a task on the list",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("completed task: ", cCtx.Args().First())
+					return nil
+				},
+			},
+			{
+				Name:    "template",
+				Aliases: []string{"t"},
+				Usage:   "options for task templates",
+				Subcommands: []*cli.Command{
+					{
+						Name:  "add",
+						Usage: "add a new template",
+						Action: func(cCtx *cli.Context) error {
+							fmt.Println("new task template: ", cCtx.Args().First())
+							return nil
+						},
+					},
+					{
+						Name:  "remove",
+						Usage: "remove an existing template",
+						Action: func(cCtx *cli.Context) error {
+							fmt.Println("removed task template: ", cCtx.Args().First())
+							return nil
+						},
+					},
+				},
+			},
 		},
 	}
 
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
+	_ = app.Run(os.Args)
 }
